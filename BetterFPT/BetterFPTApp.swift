e83a3a4f0a98645e829e561f57705edd691c2f0d
SaurabhSikka <saurabh.a.sikka@gmail.com>
2024-02-01 22:54:17 +0100

    Initial Commit

---
+++ BetterFPT/BetterFPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterFPTApp.swift
+//  BetterFPT
+//
+//  Created by Saurabh Sikka A on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterFPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterFPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterFPT
+//
+//  Created by Saurabh Sikka A on 01/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
